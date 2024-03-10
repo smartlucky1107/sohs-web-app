a810dd2fbfd5c5b9cf604a059c8d66e197184293
unknown <smartluckyman1107@gmail.com>
2024-03-10 08:02:46 -0700

    api url changed

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   // apiUrl: "http://localhost:3000/api",
-  apiUrl: "https://sohs-web-app.vercel.app/"
+  apiUrl: "https://sohs-web-app.vercel.app/api"
 };
 
 export default config;
