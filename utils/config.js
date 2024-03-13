751860bd1f6cd5526ce7d336929e107dac909f2e
unknown <smartluckyman1107@gmail.com>
2024-03-13 10:31:19 -0700

    css changed

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl: "http://localhost:3000/api",
-  // apiUrl: "https://sohs-web-app.vercel.app/api",
+  // apiUrl: "http://localhost:3000/api",
+  apiUrl: "https://sohs-web-app.vercel.app/api",
 };
 
 export default config;
